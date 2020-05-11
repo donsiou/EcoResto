9c1b40769286e43e0fed3e15e919e2d4954166e6
Hp <bousrihrim96@gmail.com>
2020-05-11 18:32:47 +0200

    getAllIngredient permet de chercher ar l'id de l'article dans la base et retourner tous les ingrédients associés

--- Sprint1/models/article.py
+++ Sprint1/models/article.py
@@ -3,19 +3,29 @@ from django.db import models
 
 class Article(models.Model):
     """docstring for Accueil"""
+    _idArticle = models.AutoField(primary_key=True)
+    _nom = models.CharField(max_length=50,none=False,blank=False)
+    _type = models.CharField(max_length=100,none=False,blank=False)
+    _quantite = models.FloatField(none=False,blank=False)
+    _image = models.ImageField(blank=False)
+    _prix = models.PositiveSmallIntegerField(none=False,blank=False)
+    _description = models.TextField()
+    _diponible = models.BooleanField(default=True)
 
     def __del__(self):
-        print("dest")# Ingredient.__del__(self)
+        print("destructeur article")
 
-    def __init__(self, nom, type,quantite,image,prix,description,diponible): #, nomIngred, prixIngred, quantiteIngred, quantiteStock, typeIngred
+    def __init__(self, nom, type,quantite,prix,diponible,image="",description=""):
         self._nom = nom
         self._type = type
         self._quantite = quantite
-        self._image = image
         self._prix = prix
-        self._description = description
         self._diponible = diponible
-        #self._ingred=Ingredient(nomIngred, prixIngred, quantiteIngred, quantiteStock, typeIngred)
+        self._image = image
+        self._description = description
+
+    def getAllIngredient(self):
+        pass
 
     def ajouter(self):
         return True
@@ -27,7 +37,9 @@ class Article(models.Model):
         return True
 
     def exist(self):
-        return True
+        if (self._diponible):
+            return True
+        return False
 
     def ajouterIngred(self, nomIngrd, quantite):
         pass
@@ -35,7 +47,7 @@ class Article(models.Model):
     def supprimerIngred(self, nomIngrd):
         pass
 
-
+    #mutateurs et accesseurs
     def getNom(self):
         return self._nom
 
