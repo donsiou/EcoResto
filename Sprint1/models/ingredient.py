ea969348c0fb5986e90390c123a3864c031de5b0
DONSIOU <oussamakhayali@gmail.com>
2020-05-28 18:26:12 +0200

    Implementation des méthode de la classe Ingredient

--- Sprint1/models/ingredient.py
+++ Sprint1/models/ingredient.py
@@ -2,42 +2,67 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    _nomIngred = models.CharField(max_length=100, null=False)
+    _nomIngred = models.CharField(max_length=100, null=False, unique=True)
     _prixIngred = models.FloatField()
-    _quantiteIngred = models.FloatField()
     _quantiteStock = models.FloatField()
     _typeIngred = models.CharField(max_length=100, null=False)
 
-    def __del__(self):
-        print("del")
+    # On a supprimer les  constrcuteurs et les destructeurs pour éviter les problémes
+    # Le constructeur pour creer un objet ingredient, il faut indiquer les attributs
+    ## i = Ingredient(_nomIngred="riz", _prixIngred=1000.0, _quantiteStock=1000.5, _typeIngred="Legume")
+    def __str__(self):
+        return self._nomIngred
 
-    def __init__(self, nomIngred, prixIngred, quantiteIngred, quantiteStock, typeIngred):
-        self._nomIngred = nomIngred
-        self._prixIngred = prixIngred
-        self._quantiteIngred = quantiteIngred
-        self._quantiteStock = quantiteStock
-        self._typeIngred = typeIngred
+    @staticmethod
+    def getByName(nomIngred):
+        i = Ingredient.objects.filter(_nomIngred__iexact=nomIngred)
+        if i.count() == 0: return None
+        return i[0]
 
+
+    # False : L'ingredient existe déja
+    # True : L'ingredient ajouté avec succé
     def ajouter(self):
+        if self.exist():
+            return False
+        self.save()
         return True
 
     def modifier(self):
+        if not self.exist():
+            return False
+        self.save()
         return True
 
     def supprimer(self):
+        if not self.exist():
+            return False
+        self.delete()
         return True
 
     def exist(self):
-        return True
+        i = Ingredient.objects.filter(_nomIngred__iexact=self._nomIngred)
+        return i.count() > 0
 
-    def epuise(self):
-        return True
+    def epuise(self, quantite=0.0):
+        return (self._quantiteStock - quantite) <= 0
 
     def ajouterStock(self, quantite):
-        pass
+        if not self.exist():
+            return False
+        self.setQuantiteStock(self._quantiteStock - quantite)
+        return True
 
+    #-1 : L'ingredient n'existe pas
+    #-2 : La quantite en stock n'est pas suffisante
+    #0 : operation faite avec succe
     def soustraireStock(self, quantite):
-        pass
+        if not self.exist():
+            return -1
+        if self.epuise():
+            return -2
+        self.setQuantiteStock(self._quantiteStock - quantite)
+        return 0
 
     def getNom(self):
         return self._nom
@@ -50,12 +75,6 @@ class Ingredient(models.Model):
 
     def setPrix(self, value):
         self._prix = value
-
-    def getQuantite(self):
-        return self._quantite
-
-    def setQuantite(self, value):
-        self._quantite = value
 
     def getQuantiteStock(self):
         return self._quantiteStock
